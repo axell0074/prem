b9980272f1e15b7ee7867845eda54622c01fdb0f
axell0074 <47213729+axell0074@users.noreply.github.com>
2019-01-31 19:57:33 +0700

    Create betaspam.sh

---
+++ betaspam.sh
@@ -0,0 +1,96 @@
+#!/system/xbin/bash
+clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
+
+toilet -f slant --gay "LiteSpam"
+sleep 1
+echo "\033[33;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[36;1m★""\033[34;1mAditya-Ploit BetaSpam""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mSpamWhatsapp"
+echo "\033[35;1m2.""\033[36;1mSpamCallTokPed"
+echo "\033[31;1m0. Keluar"
+echo "\033[37;1m81.""\033[30;1mInformasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mAditya-Ploit"
+sleep 5
+clear
+php spamwa.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mAditya-Ploit"
+sleep 5
+clear
+php call.php
+fi
+
+
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "BetaSpam"
+echo "\033[34;1mRepositori:""\033[37;1m https://github.com/axell0074"
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+echo "\033[33;1mKarya: $ALERT$"
+sleep 1
+echo "\033[32;1mVersi: Beta"
+sleep 1
+echo "\033[36;1mTeam: indoXploit"
+sleep 1
+echo "\033[31;1mFollow Instagram""\033[37;1m @adityafajri.p"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "indoXploit"
+echo "RPL "
+echo "RPLA"
+echo "RPL B"
+echo "XI RPL A"
+echo "XI RPLB B"
+echo "Rumah Savage"
+echo "SMKN 2 CIMAHI"
+echo "Aditya Fajri Putra"
+sleep 1
+echo "And All Stundents SMKN 2 CIMAHI"
+echo "\033[30;1m tunggu 5 detik"
+sleep 5
+sh betaspam.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
+
